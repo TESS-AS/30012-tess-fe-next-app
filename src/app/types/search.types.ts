32530b237f0ae130c5218dd67c8650d68f4da3f3
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-04-01 17:17:13 +0200

    axios instance done, service implementation, interface implementation, old reusable components implemented with tailwind

---
+++ src/app/types/search.types.ts
@@ -0,0 +1,22 @@
+export interface IProductSearch {
+    product_name: string;
+    product_number: string;
+    media: string;
+}
+  
+export interface ISuggestions {
+    keyword: string;
+}
+  
+  
+export interface ISearchList {
+    product: SearchArray[];
+    page: number;
+    totalPages: number;
+}
+
+export interface SearchArray {
+    product_number: string;
+    product_name: string;
+    media_m: string;
+}
