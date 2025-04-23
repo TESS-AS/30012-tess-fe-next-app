139f63c1112f74c63a4cdae778c33e8d4f6f5d43
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-04-23 10:29:02 +0200

    breadcrumb

--- src/components/category/category-content.tsx
+++ src/components/category/category-content.tsx
@@ -3,8 +3,11 @@
 import { ProductGrid } from "@/components/products/product-grid";
 import { Category } from "@/types/categories.types";
 import { IProduct } from "@/types/product.types";
+import { useParams } from "next/navigation";
+import { formatUrlToDisplayName } from "@/utils/string-utils";
 
 import { FilterCategory } from "../ui/filter";
+import { Breadcrumb } from "../ui/breadcrumb";
 
 interface CategoryContentProps {
 	products: IProduct[];
@@ -17,6 +20,25 @@ export default function CategoryContent({
 	categoryData,
 	filters,
 }: CategoryContentProps) {
+	const params = useParams();
+	const category = params.category as string;
+	const subcategory = params.subcategory as string;
+
+	const breadcrumbItems = [
+		{
+			href: `/${category}`,
+			label: formatUrlToDisplayName(category),
+		},
+		...(subcategory
+			? [
+					{
+						href: `/${category}/${subcategory}`,
+						label: formatUrlToDisplayName(subcategory),
+					},
+			  ]
+			: []),
+	];
+
 	if (!categoryData) {
 		return (
 			<div className="flex items-center justify-center py-8">
@@ -24,11 +46,15 @@ export default function CategoryContent({
 			</div>
 		);
 	}
+
 	const title = categoryData?.name || "Products";
 
 	return (
 		<div className="py-8">
-			<h1 className="mb-6 text-3xl font-bold text-gray-900">{title}</h1>
+			<div className="mb-6">
+				<Breadcrumb items={breadcrumbItems} />
+				<h1 className="mt-4 text-3xl font-bold text-gray-900">{title}</h1>
+			</div>
 			{products?.length > 0 ? (
 				<ProductGrid
 					filters={filters}
