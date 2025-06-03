327fd18d3b5441a485939e91c4099de251321419
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-06-03 15:49:38 +0400

    dimensions

--- src/services/dimensions.service.ts
+++ src/services/dimensions.service.ts
@@ -20,3 +20,73 @@ export async function createUserDimensions(payload: CreateUserDimensions): Promi
         throw error;
     }
 }
+
+export const deleteCustomerDimensions = async (customerNumber: string): Promise<{ dimensionId: number, type: number }> => {
+    try {
+        const response = await axiosInstance.delete(`/deleteCustomerDimension/${customerNumber}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error deleting dimensions:", error);
+        throw error;
+    }
+}
+
+export const getCustomerDimensions = async (customerNumber: string): Promise<{
+    d1_id: number,
+    d1_name: string,
+    d2_id: number,
+    d2_name: string,
+    d3_id: number,
+    d3_name: string
+}[]> => {
+    try {
+        const response = await axiosInstance.get(`/getCustomerDimension/${customerNumber}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error getting dimensions:", error);
+        throw error;
+    }
+}
+
+export const getUserDimensions = async (userId: number, customerNumber: string): Promise<{
+    userId: number,
+    customerId: number,
+    customerNumber: string,
+    dimension1: {
+      ids: number[],
+      label: string,
+      mode: boolean
+    },
+    dimension2: {
+      ids: number[],
+      label: string,
+      mode: boolean
+    },
+    dimension3: {
+      ids: number[],
+      label: string,
+      mode: boolean
+    }
+}> => {
+    try {
+        const response = await axiosInstance.get(`/getUserDimension/${userId}/${customerNumber}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error getting dimensions:", error);
+        throw error;
+    }
+}
+
+export const updateUserDimensions = async (userId: number, customerNumber: string, payload: {
+    dimension_1_id: number[],
+    dimension_2_id: number[],
+    dimension_3_id: number[],
+}): Promise<{ success: boolean, data: object }> => {
+    try {
+        const response = await axiosInstance.put(`/updateUserDimension/${userId}/${customerNumber}`, payload);
+        return response.data;
+    } catch (error) {
+        console.error("Error updating dimensions:", error);
+        throw error;
+    }
+}
