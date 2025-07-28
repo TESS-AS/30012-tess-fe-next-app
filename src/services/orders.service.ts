af29d328ca907035f415deabf6d6b58a4c596e54
valmir-haxholli <valmir.haxholli@gmail.com>
2025-07-28 09:56:44 +0200

    TESS-2214: type errors

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -3,7 +3,7 @@ import { Order } from "@/types/orders.types";
 import axiosInstance from "./axiosClient";
 
 interface SalesOrderResponse {
-	data?: any;
+	data?: any; //TODO: Add response type
 	order?: Order;
 }
 
