7481f7d9a11a6b9152a13dca382fc2863bbfcb46
valmir-haxholli <valmir.haxholli@gmail.com>
2025-08-22 08:08:15 +0200

    TESS-2036: build hotfix

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -108,7 +108,7 @@ function NavigationMenuViewport({
 	return (
 		<div
 			className={cn(
-				"absolute top-full left-0 isolate z-50 flex justify-center",
+				"absolute top-full left-0 isolate z-50 mt-[-1px] flex justify-center",
 			)}>
 			<NavigationMenuPrimitive.Viewport
 				data-slot="navigation-menu-viewport"
