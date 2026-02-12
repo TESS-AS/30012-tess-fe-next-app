193e80a0cc8460bfcab400914b4afcd12ee2b2df
99CCC <carlrolllund@hotmail.com>
2026-02-12 12:32:18 +0100

    baseurl axios

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -1,5 +1,5 @@
 export const baseURL =
-	"https://30011-proxyapi-cuafeua6bha7ckby.norwayeast-01.azurewebsites.net";
+	"https://api.tessix.no";
 
 export const defaultHeaders = {
 	"Content-Type": "application/json",
