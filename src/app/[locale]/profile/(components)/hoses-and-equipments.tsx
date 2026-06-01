31536de93b81d4d481d84f1f2789bd3a957ca90e
Glauk Thaqi <glaukthaqi15@gmail.com>
2026-06-01 14:04:25 +0200

    add new location in thm

--- src/app/[locale]/profile/(components)/hoses-and-equipments.tsx
+++ src/app/[locale]/profile/(components)/hoses-and-equipments.tsx
@@ -86,6 +86,7 @@ export function HosesAndEquipments({
 	const S1_CODE_VALEMON = "1291617";
 	const S1_CODE_SNORRE_A = "1291622";
 	const S1_CODE_GULLFAKS_A = "1291607";
+	const S1_CODE_SNORRE_B = "1370349";
 
 	const [isAddingToCart, setIsAddingToCart] = useState(false);
 	const [supportOpen, setSupportOpen] = useState(false);
@@ -931,7 +932,8 @@ export function HosesAndEquipments({
 							s1.S1Code === S1_CODE_DRAUPNER ||
 							s1.S1Code === S1_CODE_VALEMON ||
 							s1.S1Code === S1_CODE_SNORRE_A ||
-							s1.S1Code === S1_CODE_GULLFAKS_A,
+							s1.S1Code === S1_CODE_GULLFAKS_A ||
+							s1.S1Code === S1_CODE_SNORRE_B,
 					)
 					.map((s1) => (
 						<SelectItem
